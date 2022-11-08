2fdf34bb6bb3c2196bcfafdd7a1f535b3e48e131
Suna Lee <94081891+Ol-Or@users.noreply.github.com>
2022-11-08 22:40:31 +0900

    Update Ultrasonic.py

--- Ultrasonic.py
+++ Ultrasonic.py
@@ -10,7 +10,7 @@ ECHO = 21
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
-# 초음파 출력 초기화
+# Ultrasonic Output Reset
 GPIO.output(TRIG, False)
 time.sleep(2)
 
@@ -19,7 +19,7 @@ GPIO.setup(A1A, GPIO.OUT)
 GPIO.output(A1A, GPIO.LOW)
 
 try:
-    # 펄스 발생 딜레이
+    # Pulse delay
     while True:
         pulse_start = 0
         pulse_stop = 0
@@ -27,21 +27,21 @@ try:
         time.sleep(0.00001)
         GPIO.output(TRIG, False)
 
-        # ECHO 상승 시간값 저장
+        
         while GPIO.input(ECHO) == 0:
           pulse_start = time.time()
 
-        # ECHO 하강 시간값 저장
+        
         while GPIO.input(ECHO) == 1:
           pulse_stop = time.time()
 
         pulse_time = pulse_stop - pulse_start
-        # 물까지의 거리계산
+        
         distance = pulse_time * 17150
 
         time.sleep(1)
 
-        # 워터펌프 작동
+        # Waterpump on
         if distance > 8: 
             GPIO.output(A1A, GPIO.HIGH)
         else:
